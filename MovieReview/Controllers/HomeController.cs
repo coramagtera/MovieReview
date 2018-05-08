d4bb2a08153c9108eac633a63bec85b4b3be06ce
samuelfennell10@gmail.com <samuelfennell10@gmail.com>
2018-05-08 14:17:17 -0300

    added the template, changed the main and about page.

--- MovieReview/Controllers/HomeController.cs
+++ MovieReview/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MovieReview.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "ayo this is the application description page.";
 
             return View();
         }
